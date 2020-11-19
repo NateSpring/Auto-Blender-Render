84365464dc937c1d4229abd91a58f64e84c82752
Nate Spring <45617450+NateSpring@users.noreply.github.com>
2020-11-19 09:47:57 -0700

    File Skipping, Terminal Text Color
    -Added ability to skip filetypes that aren't .BLEND filetype.
    -Added color to terminal status updates: Yellow for skipping files/ existing folders, Green for successful file rendering.

--- auto-blender-render.py
+++ auto-blender-render.py
@@ -1,7 +1,12 @@
 import os
+import sys
 import os.path
 import subprocess
+from colorama import init, Fore, Back, Style
 
+init(convert=True)
+
+os.chdir(r'\\filestore\\network storage\\Data\\ITD\\Team-ITD-Super-Site')
 que_dir = r'\\filestore\\network storage\\Data\\ITD\\Team-ITD-Super-Site\\Images\\Product Renderings\\Render Queue\\'
 finished_dir = r'\\filestore\\network storage\\Data\\ITD\\Team-ITD-Super-Site\\Images\\Product Renderings\\Finished Renderings\\'
 
@@ -14,7 +19,11 @@ for _, folders, files in os.walk(que_dir):
 
         if os.path.exists(finished_path):
 
-            print('Folder Already Exists: SKIPPING -- %s' % folder)
+            print(Fore.YELLOW +
+                  '\n\n__________________FOLDER SKIPPED__________________\n\n')
+            print(Fore.YELLOW + 'Folder: %s -- Skipped, already exists' % (folder))
+            print(Fore.YELLOW + '\n________________________________________________\n')
+            print(Style.RESET_ALL)
             continue
 
         else:
@@ -25,17 +34,30 @@ for _, folders, files in os.walk(que_dir):
             files = os.listdir(que_dir_folders)
 
             for file in files:
-                file_counter + 1
-                file_folder = os.path.join(
-                    finished_render_folder, file.replace('.blend', ''))
+                if file.endswith('.blend'):
+                    file_counter + 1
+                    file_folder = os.path.join(
+                        finished_render_folder, file.replace('.blend', ''))
 
-                try:
+                    try:
 
-                    subprocess.call(["C:/Program Files/Blender Foundation/Blender 2.90/blender", '--background',
-                                     "\\Images\\Product Renderings\\Render Queue\\{}\\{}".format(folder, file), '-o', file_folder, '-a'])
-                    print('\n\n__________________STATUS UPDATE__________________\n                 File: %s Complete\n________________________________________________\n\n' %
-                          (file))
-                except Exception as e:
-                    print(e)
+                        subprocess.call(["C:/Program Files/Blender Foundation/Blender 2.90/blender", '--background',
+                                         "\\Images\\Product Renderings\\Render Queue\\{}\\{}".format(folder, file), '-o', file_folder, '-a'])
+                        print(Fore.GREEN +
+                              '\n\n__________________STATUS UPDATE__________________\n\n')
+                        print(Fore.GREEN + 'File: %s Complete' % (file))
+                        print(Fore.GREEN +
+                              'Congrats, have a taco with your success  🌮')
+                        print(
+                            Fore.GREEN + '\n________________________________________________\n')
+                        print(Style.RESET_ALL)
 
-
+                    except Exception as e:
+                        print(e)
+                else:
+                    print(
+                        Fore.YELLOW + '\n\n__________________SKIPPING FILE__________________\n\n')
+                    print(Fore.YELLOW + 'File: %s -- Not .BLEND filetype' % (file))
+                    print(
+                        Fore.YELLOW + '\n________________________________________________\n')
+                    print(Style.RESET_ALL)
