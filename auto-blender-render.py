6fcc9aa394b1500313e540252078dc364c58666d
Nate Spring <45617450+NateSpring@users.noreply.github.com>
2020-11-18 17:35:59 -0700

    Updated file paths and loops through all files

--- auto-blender-render.py
+++ auto-blender-render.py
@@ -1,25 +1,41 @@
-import os, os.path
+import os
+import os.path
 import subprocess
 
+que_dir = r'\\filestore\\network storage\\Data\\ITD\\Team-ITD-Super-Site\\Images\\Product Renderings\\Render Queue\\'
+finished_dir = r'\\filestore\\network storage\\Data\\ITD\\Team-ITD-Super-Site\\Images\\Product Renderings\\Finished Renderings\\'
 
-render_queue = r'\\filestore\\network storage\\Data\\ITD\\Team-ITD-Super-Site\\Images\\Product Renderings\\Render Queue\\'
-finished_renders = r'\\filestore\\network storage\\Data\\ITD\\Team-ITD-Super-Site\\Images\\Product Renderings\\Finished Renderings\\'
+for _, folders, files in os.walk(que_dir):
 
-for _, folders, files in  os.walk(render_queue):
-    
     for folder in folders:
-        
-        render_queue_folders = os.path.join(root_path, folder)
-        finish_path = os.path.join(finished_renders, folder)
 
-        files = os.listdir(render_queue_folders)
+        que_dir_folders = os.path.join(que_dir, folder)
+        finished_path = os.path.join(finished_dir, folder)
 
-        for file in files:
-            os.mkdir(finish_path)
-            inside_finish_path = finish_path + "\\\\yes_queeeeeeeeeen"
-            subprocess.call(["C:/Program Files/Blender Foundation/Blender 2.90/blender", '--background', "\\Images\\Product Renderings\\Render Queue\\{}\\{}".format(folder, file), '-o', finish_path, '-a'])
+        if os.path.exists(finished_path):
 
-            
+            print('Folder Already Exists: SKIPPING -- %s' % folder)
+            continue
 
+        else:
+
+            file_counter = 1
+            os.mkdir(finished_path)
+            finished_render_folder = finished_path + "\\\\"
+            files = os.listdir(que_dir_folders)
+
+            for file in files:
+                file_counter + 1
+                file_folder = os.path.join(
+                    finished_render_folder, file.replace('.blend', ''))
+
+                try:
+
+                    subprocess.call(["C:/Program Files/Blender Foundation/Blender 2.90/blender", '--background',
+                                     "\\Images\\Product Renderings\\Render Queue\\{}\\{}".format(folder, file), '-o', file_folder, '-a'])
+                    print('\n\n__________________STATUS UPDATE__________________\n                 File: %s Complete\n________________________________________________\n\n' %
+                          (file))
+                except Exception as e:
+                    print(e)
 
 
